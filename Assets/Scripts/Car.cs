777edba62644480a4c2c8eb442a2dbd68cd2e704
Maksym <eldiablo@ukr.net>
2022-06-07 14:59:54 +0300

    car movement

---
+++ Assets/Scripts/Car.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public class Car : MonoBehaviour
+{
+    [SerializeField] float speed;
+    [SerializeField] float speedGain = 0.2f;
+    [SerializeField] float turnSpeed = 200f;
+
+    int steerValue;
+    void Update()
+    {
+        speed += speedGain * Time.deltaTime;
+
+        transform.Rotate(0, steerValue * turnSpeed * Time.deltaTime, 0);
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Obstacle"))
+        {
+#if UNITY_EDITOR
+            EditorApplication.ExitPlaymode();
+#else
+            Application.Quit(); 
+#endif
+            //SceneManager.LoadScene(0);
+        }
+    }
+    public void Steer(int value)
+    {
+        steerValue = value;
+    }
+}
